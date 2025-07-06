2099cd44596319d4f7ddd79f5a16ccf436a1d686
Dyan Alencar de Oliveira <dyanalencar@gmail.com>
2025-07-06 04:48:10 -0300

    Desafio Nivel Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,6 +10,29 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
+    int tabuleiro[10][10] = {0}; // Declara e Inicializa o tabuleiro 5x5 com zeros
+
+    int navio1 = 3;
+    int navio2 = 3;
+    
+    tabuleiro[1][1] = navio1; // Posiciona um navio verticalmente
+    tabuleiro[1][2] = navio1;
+    tabuleiro[1][3] = navio1;
+
+    tabuleiro[8][5] = navio2; // Posiciona um navio horizontalmente
+    tabuleiro[7][5] = navio2; // Posiciona um navio horizontalmente
+    tabuleiro[6][5] = navio2; // Posiciona um navio horizontalmente
+
+
+
+
+printf("Tabuleiro:\n");
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
